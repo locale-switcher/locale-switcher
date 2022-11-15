e24d502482f7070cf247320e4b8295f8ebbe1d53
cupcakearmy <hi@nicco.io>
2022-11-15 16:20:55 +0100

    update example dependencies

--- example/index.js
+++ example/index.js
@@ -1,16 +1,17 @@
-const path = require("path");
-const express = require("express");
-const app = express();
+import { dirname, join } from 'path'
+import express from 'express'
+import { fileURLToPath } from 'url'
+const app = express()
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs')
+app.set('views', join(dirname(fileURLToPath(import.meta.url)), 'views'))
 
-app.get("/", function (req, res) {
-  res.render("pages/index", {
-    acceptLanguage: req.headers["accept-language"] || "Not specified",
-  });
-});
+app.get('/', function (req, res) {
+  res.render('pages/index', {
+    acceptLanguage: req.headers['accept-language'] || 'Not specified',
+  })
+})
 
 app.listen(8080, function () {
-  console.log("Server listening on port 8080...");
-});
+  console.log('Server listening on port 8080...')
+})
